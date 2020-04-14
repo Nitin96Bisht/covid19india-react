4f5e65d76e9d847b58905c8db01a3d301b4bff8b
Nitin96Bisht <nitin.bisht96@gmail.com>
2020-04-14 09:25:03 +0000

    Deploying to gh-pages from  @ f47985383eda6967f759a0cd1a61a0f5b040870f 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12b39c08c6a1c20db6ce74733eedc039.js"
+  "/precache-manifest.51e75fbe2760c8aaa824657d6ebf4225.js"
 );
 
 self.addEventListener('message', (event) => {
